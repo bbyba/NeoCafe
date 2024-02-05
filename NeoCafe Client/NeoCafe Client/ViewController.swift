23079e6e9fb7f7f1bcb376d0aa6e3cb2c8b01d53
Burte Bayarsaikhan <burte.bsn@gmail.com>
2024-02-05 18:18:49 +0800

    Initial commit with Client and Staff projects

---
+++ NeoCafe Client/NeoCafe Client/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NeoCafe Client
+//
+//  Created by Burte Bayaraa on 2024.02.05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
